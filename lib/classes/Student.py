00cf622c1c35c297d6c00bb8d5148f7a833185b9
kimaniMbatiaMoringa <kimani.mbatia@student.moringaschool.com>
2023-09-11 20:50:26 +0300

    Functioning query for students added

--- lib/classes/Student.py
+++ lib/classes/Student.py
@@ -1,6 +1,32 @@
 import functools
 
-class Student:
+from sqlalchemy import (create_engine, desc, func,
+    CheckConstraint, PrimaryKeyConstraint, UniqueConstraint,
+    Index, Column, DateTime, Integer, String)
+from sqlalchemy.orm import sessionmaker
+
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+class Student(Base):
+    __tablename__='students'
+
+    """ __table_args__=(
+        PrimaryKeyConstraint(
+            'id',
+            name='id_pk'
+        ),
+        UniqueConstraint(
+            'name',
+            name='unique_name'
+        )
+    ) """
+
+    id = Column(Integer(),primary_key=True)
+    name = Column(String())
+    scores = Column(Integer())
+
+
     def __init__(self, name):
         self.name = name
         self.scores = []
